b9c32dc3536a76e54e3f359528b11a35bd86583c
dertovich <huni_19@mail.ru>
2023-07-29 21:30:29 +0300

    Start

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+
+    public static void task1() {}
+
+    public static void task2() {}
+
+    public static void task3() {}
+
+    public static void task4() {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
